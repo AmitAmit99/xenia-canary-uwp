0f6241e9dd1750b5fbb272378c998bedcd317c69
sirmangler <thecatsmangler@gmail.com>
2023-02-19 19:00:37 +0000

    Re-enable resolution scaling, and use DXGI stretch to workaround the zoom bug.

--- src/xenia/ui/imgui_drawer.cc
+++ src/xenia/ui/imgui_drawer.cc
@@ -616,6 +616,9 @@ void ImGuiDrawer::Draw(UIDrawContext& ui_draw_context) {
       float(window_->GetMediumDpi()) / float(window_->GetDpi());
   io.DisplaySize.x = window_->GetActualPhysicalWidth() * physical_to_logical;
   io.DisplaySize.y = window_->GetActualPhysicalHeight() * physical_to_logical;  
+  io.DisplayFramebufferScale.x = ((float) io.DisplaySize.x / 1920.0f) * 2.4f;
+  io.DisplayFramebufferScale.y = ((float) io.DisplaySize.y / 1080.0f) * 2.4f;
+  io.FontGlobalScale = ((float) io.DisplaySize.x / 1920.0f) * 2.4f; 
 
   if (!dialogs_.empty()) {
     UpdateGamepads();
